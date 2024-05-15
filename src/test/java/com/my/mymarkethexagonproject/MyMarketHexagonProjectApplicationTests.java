da76e89e051e55227b37c8e045deaced17b430aa
saeinlee <gitSaein@email.com>
2024-05-15 21:41:19 +0900

    Initial commit

---
+++ src/test/java/com/my/mymarkethexagonproject/MyMarketHexagonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.mymarkethexagonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketHexagonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
